400cccde9537292713fccff67977715292cabc3e
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2023-06-02 19:27:19 +0200

    Add react-query and otehr missing deps

--- src/utils/axiosUtils.js
+++ src/utils/axiosUtils.js
@@ -1,12 +1,14 @@
-import axios from "axios"
+import axios from "axios";
 
-const client = axios.create({ baseURL: "https://jsonplaceholder.typicode.com" })
+const client = axios.create({
+  baseURL: "https://jsonplaceholder.typicode.com",
+});
 
 export const request = ({ ...options }) => {
-    client.defaults.headers.common.Authorization = `Bearer token`
-    const onSuccess = response => response
-    const onError = error => {
-        return error
-    }
-    return client(options).then(onSuccess).catch(onError)
-}
+  client.defaults.headers.common.Authorization = `Bearer token`;
+  const onSuccess = (response) => response;
+  const onError = (error) => {
+    return error;
+  };
+  return client(options).then(onSuccess).catch(onError);
+};
